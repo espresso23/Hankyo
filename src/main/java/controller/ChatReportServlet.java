d011984ff6d9d77622478fcfdf79f8c5fd902283
TraviSst123 <vuminhtrietkingofwar@gmail.com>
2025-05-02 21:35:05 +0700

    xong r

--- src/main/java/controller/ChatReportServlet.java
+++ src/main/java/controller/ChatReportServlet.java
@@ -17,7 +17,7 @@ import java.util.Enumeration;
 import org.json.JSONObject;
 
 @WebServlet("/ChatReportServlet")
-public class ChatReportServlet extends HttpServlet {
+public class gChatReportServlet extends HttpServlet {
     private ReportDAO reportDAO;
 
     @Override
